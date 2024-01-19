f15e69e35ceb62b0b5b9210b9b99ddd467c9bbce
Commander M <128257558+Commander-M@users.noreply.github.com>
2024-01-19 17:46:26 +0100

    Update Email2Username.cpp

--- Email2Username.cpp
+++ Email2Username.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main()
 {
-    string email = "pranayarout88@gmail.com";
+    string email = "yourmail@gmail.com";
     int i = (int) email.find('@');
     string uname = email.substr(0, i);
     cout << "Email is " << email << endl;
